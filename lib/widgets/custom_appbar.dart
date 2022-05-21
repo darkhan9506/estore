0af89fb45037711193498eea1407253a4d1b16e0
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-05-21 21:08:03 +0600

    AppBar, NavBar

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  final String title;
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        child: Text(
+          'Dara Store',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontFamily: 'Avenir',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      iconTheme: IconThemeData(color: Colors.black),
+      actions: [IconButton(onPressed: () {}, icon: Icon(Icons.favorite))],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(50.0);
+}
