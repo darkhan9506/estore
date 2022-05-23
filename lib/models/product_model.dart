0848059c7c1487aedfaa8b181d9ae41427e1dae5
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-05-23 23:31:11 +0600

    Product carousel with Stack and LiswView part 1

---
+++ lib/models/product_model.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+
+  const Product({
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.price,
+    required this.isRecommended,
+    required this.isPopular,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        name,
+        category,
+        imageUrl,
+        price,
+        isRecommended,
+        isPopular,
+      ];
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -20,18 +20,23 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(title: "XoXo Brands"),
       bottomNavigationBar: CustomNavBar(),
-      body: Container(
-        child: CarouselSlider(
-          options: CarouselOptions(
-            aspectRatio: 1.5,
-            viewportFraction: 0.9,
-            enlargeCenterPage: true,
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
+      body: Column(
+        children: [
+          Container(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 1.5,
+                viewportFraction: 0.9,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+              ),
+              items: Category.categories
+                  .map((category) => HeroCorouselCard(category: category))
+                  .toList(),
+            ),
           ),
-          items: Category.categories
-              .map((category) => HeroCorouselCard(category: category))
-              .toList(),
-        ),
+          SectionTitle(title: 'RECOMMENDED'),
+        ],
       ),
     );
   }
